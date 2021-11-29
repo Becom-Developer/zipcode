7502ab431603d7139d94bd47ff2081bf091ccbef
yk <yosizuka1@gmail.com>
2021-11-29 18:00:28 +0900

    test

--- zsearch.pl
+++ zsearch.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use utf8;
 use Encode qw(encode decode);
+use Text::CSV;
 for my $byte_text (@ARGV) {
     my $text     = decode( 'UTF-8', $byte_text );
     my $count    = length $text;
@@ -10,8 +11,27 @@ for my $byte_text (@ARGV) {
     my $byte_msg = encode( 'UTF-8', $msg );
     print $byte_msg;
 }
-my $path = './cpanfile';
-open my $fh, '<', $path or die $!;
-for my $lint (<$fh>) {
-    print $lint;
+my $path = './csv/40FUKUOK.CSV';
+
+# open my $fh, '<', $path or die $!;
+# for my $lint (<$fh>) {
+#     print $lint;
+# }
+
+my @rows;
+my $address;
+# Read/parse CSV
+my $csv = Text::CSV->new();
+open my $fh, "<:encoding(utf8)", $path or die "test.csv: $!";
+while ( my $row = $csv->getline($fh) ) {
+    my $zipcode = $row->[2];
+    if ($zipcode eq 8120041) {
+        push @rows, $row;
+    }
 }
+
+# my $byte_address = encode( 'UTF-8', @rows[0][0] );
+
+# print $byte_address."\n";
+close $fh;
+
