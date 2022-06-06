faa5a9e70b4b64e65da4d7622858261167b34ca8
yk <yosizuka1@gmail.com>
2022-06-06 15:19:36 +0900

    test

--- lib/Zsearch/CGI.pm
+++ lib/Zsearch/CGI.pm
@@ -21,43 +21,43 @@ sub run {
     # apikeyのdbができてから実装
     # my $cookie_apikey = $query->cookie('apikey');
 
-    my $origin  = $ENV{HTTP_ORIGIN};
-    my @headers = (
-        -type    => 'application/json',
-        -charset => 'utf-8',
-    );
-    if ($origin) {
-        @headers = (
-            @headers,
-            -access_control_allow_origin  => $origin,
-            -access_control_allow_headers => 'content-type,X-Requested-With',
-            -access_control_allow_methods => 'GET,POST,OPTIONS',
-            -access_control_allow_credentials => 'true',
-        );
-    }
-    $self->render->raw( $q->header(@headers) );
-    my $opt      = {};
-    my $postdata = $q->param('POSTDATA');
-    if ($postdata) {
-        $opt = decode_json($postdata);
-    }
-    warn 'Zsearch::CGI----2';
+    # my $origin  = $ENV{HTTP_ORIGIN};
+    # my @headers = (
+    #     -type    => 'application/json',
+    #     -charset => 'utf-8',
+    # );
+    # if ($origin) {
+    #     @headers = (
+    #         @headers,
+    #         -access_control_allow_origin  => $origin,
+    #         -access_control_allow_headers => 'content-type,X-Requested-With',
+    #         -access_control_allow_methods => 'GET,POST,OPTIONS',
+    #         -access_control_allow_credentials => 'true',
+    #     );
+    # }
+    # $self->render->raw( $q->header(@headers) );
+    # my $opt      = {};
+    # my $postdata = $q->param('POSTDATA');
+    # if ($postdata) {
+    #     $opt = decode_json($postdata);
+    # }
+    # warn 'Zsearch::CGI----2';
 
-    # Validate
-    return $self->error->output(
-        "Unknown option specification: resource, method, apikey")
-      if !$opt->{resource} || !$opt->{method} || !$opt->{apikey};
-    return $self->error->output("apikey is incorrect: $opt->{apikey}")
-      if $apikey ne $opt->{apikey};
-    warn 'Zsearch::CGI----3';
+    # # Validate
+    # return $self->error->output(
+    #     "Unknown option specification: resource, method, apikey")
+    #   if !$opt->{resource} || !$opt->{method} || !$opt->{apikey};
+    # return $self->error->output("apikey is incorrect: $opt->{apikey}")
+    #   if $apikey ne $opt->{apikey};
+    # warn 'Zsearch::CGI----3';
 
-    # Routing
-    if ( $opt->{resource} eq 'search' ) {
-        my $output = $self->sql->run($opt);
-        $self->render->all_items_json($output);
-        return;
-    }
-        warn 'Zsearch::CGI----4';
+    # # Routing
+    # if ( $opt->{resource} eq 'search' ) {
+    #     my $output = $self->sql->run($opt);
+    #     $self->render->all_items_json($output);
+    #     return;
+    # }
+    #     warn 'Zsearch::CGI----4';
 
     return $self->error->output("The resource is specified incorrectly");
 }
